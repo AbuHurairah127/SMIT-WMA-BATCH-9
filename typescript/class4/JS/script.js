98f7b9c2b44e32c690912a060a4f185882686ecd
abuhurairah127 <dev.abuhurairah@gmail.com>
2026-02-27 08:30:11 +0500

    code

---
+++ typescript/class4/JS/script.js
@@ -0,0 +1,25 @@
+console.log("Hello World in TS folder");
+console.log("Hello World in TS folder");
+console.log("Hello World in TS folder");
+console.log("Hello World 2nd time in TS folder");
+console.log("Hello World 2nd time in TS folder");
+console.log("Hello World 2nd time in TS folder");
+console.warn("Hello World 3rd time in TS folder");
+console.error("Hello World 3rd time in TS folder");
+// alert("Hello World 4th time in TS folder");
+// prompt("Enter your name");
+// confirm("Are you sure you want to delete this file?");
+var myName = prompt("Enter your name");
+// console.log(myName?.trim());
+// console.log(myName?.trimStart());
+// console.log(myName?.trimEnd());
+// console.log(myName.toLowerCase());
+// console.log(myName.toUpperCase());
+// console.log(myName?.length);
+// console.log(myName?.replace("  ",""))
+// console.log(myName?.replaceAll("  ",""))
+console.log(myName);
+console.log(myName?.charAt(0));
+export {};
+// 33100-1000000-1
+//# sourceMappingURL=script.js.map

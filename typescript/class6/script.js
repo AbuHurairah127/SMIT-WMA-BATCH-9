5069fd2b6c20589c436e7a3e4ccab5a2eed964e8
abuhurairah127 <dev.abuhurairah@gmail.com>
2026-04-10 16:16:18 +0500

    class6

---
+++ typescript/class6/script.js
@@ -0,0 +1,22 @@
+"use strict";
+var student1 = "Abu Hurairah";
+var student2 = "Anas";
+var student3 = "Bilal";
+var student4 = "Zahid";
+// Array
+var students = ["Abu Hurairah", "Anas", "Bilal", "Zahid"]; // ✅
+// var students2 =["Abu Hurairah",3207443537,"abuhurairah127@gmail.com",23,true] // ❌
+// var students3 = new Array("Abu Hurairah", "Anas", "Bilal", "Zahid") // ✅
+// console.log(students3)
+// console.log(students[1])
+// console.log(students[2])
+// console.log(students[3])
+var num = 2;
+console.log(students[1], students[num], students[3]);
+console.log(students.length);
+// Last index of the array = length - 1
+console.log(students[1]);
+// char
+// char[] = string
+var myName = "Abu Hurairah";
+console.log("🚀 ~ myName:", myName[0]);
---
+++ typescript/class6/script.ts
@@ -0,0 +1,25 @@
+var student1:string = "Abu Hurairah"
+var student2:string = "Anas"
+var student3:string = "Bilal"
+var student4:string = "Zahid"
+
+// Array
+var students = ["Abu Hurairah", "Anas", "Bilal", "Zahid"] // ✅
+// var students2 =["Abu Hurairah",3207443537,"abuhurairah127@gmail.com",23,true] // ❌
+// var students3 = new Array("Abu Hurairah", "Anas", "Bilal", "Zahid") // ✅
+// console.log(students3)
+
+// console.log(students[1])
+// console.log(students[2])
+// console.log(students[3])
+var num = 2
+console.log(students[1],students[num],students[3])
+console.log(students.length)
+
+// Last index of the array = length - 1
+console.log(students[1])
+
+// char
+// char[] = string
+var myName:string = "Abu Hurairah"
+console.log("🚀 ~ myName:", myName[0])
